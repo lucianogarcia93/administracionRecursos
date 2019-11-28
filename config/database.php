ca0908f67be66f728ba592a12670b1eee4e33bc2
francoing <franco.montti.19@gmail.com>
2019-11-28 15:16:03 -0300

    modificado config para base de datos en heroku

--- config/database.php
+++ config/database.php
@@ -1,5 +1,6 @@
 <?php
-
+$DATABASE_URL= parse_url('postgres: // mxnkclquxzekyk : 01f892b2a584947e8859950135fe5c9fa3ef59c87ac883f7da264600c0390ca4 @ ec2-54-221-201-212.compute-1.amazonaws.com : 5432 / db2mq42ubd2qjf
+');
 return [
 
     /*
@@ -13,7 +14,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,11 +57,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL['host'],
+            'port' => $DATABASE_URL['port'],
+            'database' => ltrim($DATABASE_URL["path"],"/"),
+            'username' => $DATABASE_URL['user'],
+            'password' => $DATABASE_URL['pass'],
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
