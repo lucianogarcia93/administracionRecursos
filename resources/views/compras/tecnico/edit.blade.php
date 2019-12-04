aedcc9bbe287d9ee4cfe3863c88458cc00b842d4
escorpio8926 <escorpio_89_32@hotmail.com>
2019-12-03 22:29:02 -0300

    editar Tecnico

--- resources/views/compras/tecnico/edit.blade.php
+++ resources/views/compras/tecnico/edit.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
 
-            <h3>Editar Tecnico:{{$persona -> nombre}}</h3>
+            <h3>Editar Tecnico:{{$tecnico -> nombre}}</h3>
 
                 @if(count($errors) > 0)
                 <div class="alert alert-danger">
@@ -20,7 +20,7 @@
                 @endif
         </div>
    </div>
-                {!!Form::model($persona,['method'=>'PATCH','route'=>['tecnico.update',$persona->idpersona]])!!}
+                {!!Form::model($tecnico,['method'=>'PATCH','route'=>['tecnico.update',$tecnico->idtecnico]])!!}
                 {{Form::token()}}
 
                 <div class="row">
@@ -28,7 +28,7 @@
                     <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
                         <div class="form-group">
                             <label for="nombre" class="">Nombre</label>
-                            <input type="text" name="nombre" required value="{{$persona->nombre}}" class="form-control "placeholder="Nombre...">
+                            <input type="text" name="nombre" required value="{{$tecnico->nombre}}" class="form-control "placeholder="Nombre...">
                         </div>
                     </div>
 
@@ -36,7 +36,7 @@
                     <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
                         <div class="form-group">
                                 <label for="telefono" class="">Telefono</label>
-                                <input type="text" name="telefono"  value="{{$persona->telefono}}" class="form-control "placeholder="Numero de telefono..." >
+                                <input type="text" name="telefono"  value="{{$tecnico->telefono}}" class="form-control "placeholder="Numero de telefono..." >
                         </div>
                     </div>
 
@@ -45,21 +45,21 @@
                     <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
                         <div class="form-group">
                                 <label for="email" class="">Email</label>
-                                <input type="text" name="email" value="{{$persona->email}}" class="form-control "placeholder="Email..." >
+                                <input type="text" name="email" value="{{$tecnico->email}}" class="form-control "placeholder="Email..." >
                         </div>
                     </div>
 
-                                    
+
 
 
                     <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
                     <div class="form-group">
-                        
+
                         <button type="submit" class="btn btn-primary">Guardar</button>
-                        
+
                         <button type="reset" class="btn btn-danger">Cancelar</button>
-                        
-                        
+
+
                     </div>
                     </div>
 
